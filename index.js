d6c4d2bb8edfeb2e98da420c32b2d497dad09a3b
Anon <jew.bilkis@gmail.com>
2016-12-31 02:52:57 +0300

    changes for a sample

--- index.js
+++ index.js
@@ -3,14 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.get('/',function(req,res){
+       
+  res.sendFile(__dirname + '/index.html');
 
-// views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
