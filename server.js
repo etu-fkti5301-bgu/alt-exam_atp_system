bd75762b8ebc323b018b517c4402ae46b56730b2
andixlm <andixev@gmail.com>
2017-01-01 17:08:45 +0300

    server: Fix client/server data exchange

--- server.js
+++ server.js
@@ -5,32 +5,34 @@ var child       = require('child_process');
 var app         = express();
 
 app.set('port', (process.env.PORT || 5000));
-
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.listen(app.get('port'), function () {
-  console.log('[LOG.INFO]: Server has started. Listening on port ' + app.get('port') + '.');
-});
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
-	console.log('[LOG.INFO]: Connection to server.')
+    res.sendFile(__dirname + '/index.html');
+    console.log('[LOG.INFO]: Connection to server.')
 });
 
 app.post('/', function(req, res) {
-	var atp = child.spawn('node', ['./js/atp/core.js', req.body.formulainput]);
-	atp.stdout.on('data', function(data) {
-		console.log('[LOG.INFO]: Received message: ' + req.body.formulainput);
-		var html = '<h3>Results:</h3><p>' + data + '</p><br/><a href="/">Try again</a>';
-		res.send(html);
-		res.end();
-		console.log('[LOG.INFO]: Results sent to client.');
-  	});
+    var atp = child.spawn('node', ['./js/atp/core.js', req.body.message]);
+    console.log('[LOG.INFO]: Received message: ' +
+      '\"' + req.body.message + '\".');
+
+    atp.stdout.on('data', function(data) {
+        var html = '<h3>Results:</h3><p>' + data + '</p><br/><a href="/">Try again</a>';
+
+        res.send(html);
+        res.end();
+
+        console.log('[LOG.INFO]: Correct received message. Results sent to client.');
+    });
 
   atp.stderr.on('data', function(data) {
-	console.log('[LOG.INFO]: Received message: ' + req.body.formulainput);
-	res.end();
-	console.log('[LOG.ERROR]: Received message isn\'t correct.')
+    res.end();
+
+    console.log('[LOG.ERROR]: Incorrect received message. Abort.');
   });
+});
+
+app.listen(app.get('port'), function () {
+  console.log('[LOG.INFO]: Server has started. Listening on port ' + app.get('port') + '.');
 });
